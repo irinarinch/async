cc9d2624cb063ce8be0518035d1289fbaff4b6b4
irinarinch <irinarinch@gmail.com>
2023-05-26 18:36:17 +0800

    fix lin errors

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -2,11 +2,11 @@ import json from './parser';
 import read from './reader';
 import GameSaving from './GameSaving';
 
-export default class GameSavingLoader {  
+export default class GameSavingLoader {
   static async load() {
     const data = await read();
     const saving = await json(data);
     const savingObj = JSON.parse(saving);
     return new GameSaving(savingObj.id, savingObj.created, savingObj.userInfo);
-  } 
+  }
 }
